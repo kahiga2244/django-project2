36447ba33a26abf26d7f37330d0d866f8ca0ad31
kahiga2244 <kahigakamiru@gmail.com>
2021-04-08 00:23:28 +0300

    adding the signup form

--- myawards/views.py
+++ myawards/views.py
@@ -55,3 +55,30 @@ class UserViewSet(viewsets.ModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            messages.success(request, 'Account was created.')
+            return HttpResponse('We have sent you an email, please confirm your email address to complete registration')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
