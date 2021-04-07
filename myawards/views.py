411260bde0929525673a9c7dd197fc2bbf140206
kahiga2244 <kahigakamiru@gmail.com>
2021-04-08 00:16:14 +0300

    importing models and classes

--- myawards/views.py
+++ myawards/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm
+from rest_framework import viewsets
+from .models import Profile, Post, Rating
+from .serializers import ProfileSerializer, UserSerializer, PostSerializer
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+import random
