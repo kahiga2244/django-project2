b3bfc681a9db9f86cc5dece262e2516c41783f77
kahiga2244 <kahigakamiru@gmail.com>
2021-04-09 09:23:42 +0300

    putting up the ratings and update forms

--- myawards/forms.py
+++ myawards/forms.py
@@ -26,3 +26,14 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio', 'contact']
+
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['design', 'usability', 'content']
