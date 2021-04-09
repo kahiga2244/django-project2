4e6eddea8893c60d2c957e1a5957a69bd4bc150b
kahiga2244 <kahigakamiru@gmail.com>
2021-04-09 09:20:22 +0300

    creating class User and Post Serializers

--- myawards/serializers.py
+++ myawards/serializers.py
@@ -7,3 +7,18 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'profile', 'posts']
